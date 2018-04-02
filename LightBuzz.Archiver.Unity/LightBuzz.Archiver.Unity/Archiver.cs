711e1e98e43bf70014cebd20c7696ddf9c5b61b6
Vangos Pterneas <vangos@lightbuzz.com>
2018-04-02 14:59:46 +0300

    Created an Archiver for Unity3D
    Created an Archiver for Unity3D 2017.3 or later.

---
+++ LightBuzz.Archiver.Unity/LightBuzz.Archiver.Unity/Archiver.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace LightBuzz.Archiver
+{
+    /// <summary>
+    /// Compresses and decompresses single files and folders.
+    /// </summary>
+    public static class Archiver
+    {
+        public static readonly string Extension = ".zip";
+
+        /// <summary>
+        /// Compresses a folder (including its subfolders) or file.
+        /// </summary>
+        /// <param name="source">The fodler or file to compress.</param>
+        /// <param name="destination">The compressed zip file.</param>
+        /// <param name="replaceExisting">Choose whether you'll replace the file if it already exists.</param>
+        public static void Compress(string source, string destination, bool replaceExisting = true)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentNullException("Source path is null or empty.");
+            }
+
+            if (string.IsNullOrEmpty(destination))
+            {
+                throw new ArgumentNullException("Destination path is null or empty.");
+            }
+
+            if (File.GetAttributes(destination) == FileAttributes.Directory)
+            {
+                throw new ArgumentException("Destination is a directory. You need to specify the name of a zip file, instead.");
+            }
+
+            if (replaceExisting && File.Exists(destination))
+            {
+                File.Delete(destination);
+            }
+
+            if (File.GetAttributes(source) == FileAttributes.Directory)
+            {
+                // Compress a folder.
+                ZipFile.CreateFromDirectory(source, destination);
+            }
+            else
+            {
+                // Compress a file.
+                FileInfo file = new FileInfo(source);
+
+                using (FileStream originalFileStream = file.OpenRead())
+                {
+                    if (file.Attributes != FileAttributes.Hidden & file.Extension != Extension)
+                    {
+                        using (FileStream compressedFileStream = File.Create(destination))
+                        {
+                            using (GZipStream compressionStream = new GZipStream(compressedFileStream, CompressionMode.Compress))
+                            {
+                                originalFileStream.CopyTo(compressionStream);
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Compresses a folder, including all of its files and sub-folders.
+        /// </summary>
+        /// <param name="source">The folder containing the files to compress.</param>
+        /// <param name="destination">The compressed zip file.</param>
+        /// <param name="replaceExisting">Choose whether you'll replace the file if it already exists.</param>
+        public static void Compress(DirectoryInfo source, FileInfo destination, bool replaceExisting = true)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("Source directory is null.");
+            }
+
+            if (destination == null)
+            {
+                throw new ArgumentNullException("Destination file is null.");
+            }
+
+            Compress(source.FullName, destination.FullName, replaceExisting);
+        }
+
+        /// <summary>
+        /// Compresses a single file.
+        /// </summary>
+        /// <param name="source">The file to compress.</param>
+        /// <param name="destination">The compressed zip file.</param>
+        /// <param name="replaceExisting">Choose whether you'll replace the file if it already exists.</param>
+        public static void Compress(FileInfo source, FileInfo destination, bool replaceExisting)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("Source directory is null.");
+            }
+
+            if (destination == null)
+            {
+                throw new ArgumentNullException("Destination file is null.");
+            }
+
+            Compress(source.FullName, destination.FullName, replaceExisting);
+        }
+
+        /// <summary>
+        /// Decompresses the specified file to the specified folder.
+        /// </summary>
+        /// <param name="source">The compressed zip file.</param>
+        /// <param name="destination">The directory where the file will be decompressed.</param>
+        public static void Decompress(string source, string destination)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentNullException("Source file path is null or empty.");
+            }
+
+            if (string.IsNullOrEmpty(destination))
+            {
+                throw new ArgumentNullException("Destination path is null or empty.");
+            }
+
+            if (File.GetAttributes(source) == FileAttributes.Directory)
+            {
+                throw new ArgumentException("Source is a directory. You need to specify the name of a zip file, instead.");
+            }
+
+            if (File.GetAttributes(destination) != FileAttributes.Directory)
+            {
+                throw new ArgumentException("Destination is not a directory. You need to specify the name of a directory, instead.");
+            }
+
+            if (!File.Exists(source))
+            {
+                throw new FileNotFoundException("Source file does not exist.");
+            }
+
+            if (!Directory.Exists(destination))
+            {
+                throw new DirectoryNotFoundException("Directory does not exist.");
+            }
+
+            ZipFile.ExtractToDirectory(source, destination);
+        }
+
+        /// <summary>
+        /// Decompresses the specified file to the specified folder.
+        /// </summary>
+        /// <param name="source">The compressed zip file.</param>
+        /// <param name="destination">The directory where the file will be decompressed.</param>
+        public static void Decompress(FileInfo source, DirectoryInfo destination)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("Source file is null.");
+            }
+
+            if (destination == null)
+            {
+                throw new ArgumentNullException("Destination directory is null.");
+            }
+
+            Decompress(source.FullName, destination.FullName);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,72 @@
-# Archiver-Unity
-Compress and decompress files and folders in Unity3D.
+# Archiver for Unity3D
+
+Archiver for Unity3D is a powerful utility for compressing and decompressing files and folders in Unity 2017.3 or later. Moreover, using the built-in mechanism, you couldn't compress a folder with its subfolders and files.
+
+Archiver makes compressing and decompressing amazingly easy, using just two lines of code:
+
+```
+Archiver.Compress(source, destination);   // Zip
+Archiver.Decompress(source, destination); // Unzip
+```
+        
+Where source and destination are the specified files and folders. You can run the included demo and test by yourselves.
+
+## Installation
+
+* Build the project in Visual Studio 2017 or later.
+* Drag and drop the ```LightBuzz.Archiver.dll``` to your Asset folder.
+
+## Compatibility
+
+The archiving functionality is compatible with **Unity 2017.3** or later version. The following platforms are supported:
+
+* iOS
+* Android
+* Windows Standalone (x86 and x64)
+* Universal Windows Platform (UWP)
+
+## Examples
+
+To use the Archiver, first include a reference to the assembly:
+
+```
+using LightBuzz.Archiver;
+```
+
+### Compressing a single file
+
+```
+string source = @"C:\Users\LightBuzz\Desktop\Foo.txt";
+string destination = @"C:\Users\LightBuzz\Desktop\Foo.zip";
+
+Archiver.Compress(source, destination);
+```
+    
+### Compressing a folder with all of its files and subfolders
+
+```
+string source = @"C:\Users\LightBuzz\Desktop\Foo";
+string destination = @"C:\Users\LightBuzz\Desktop\Foo.zip";
+
+Archiver.Compress(source, destination);
+```
+    
+### Decompressing a zip file
+
+```
+string source = @"C:\Users\LightBuzz\Desktop\Foo.zip";
+string destination = @"C:\Users\LightBuzz\Desktop\";
+
+Archiver.Compress(source, destination);
+```
+    
+That's it!
+
+## Contributors
+* [Vangos Pterneas](http://pterneas.com) from [LightBuzz](http://lightbuzz.com)
+
+## License
+You are free to use these libraries in personal and commercial projects by attributing the original creator. Licensed under [Apache v2 License](https://github.com/LightBuzz/archiver-unity/blob/master/LICENSE).
+
+## Support Archiver
+Do you use Archiver in your projects? Do you find it helpful? [Buy us a beer](https://paypal.me/lightbuzz)!
